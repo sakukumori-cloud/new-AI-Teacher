dc59aa9e95c009222d824c03423c20e4aab00aca
sakukumori-cloud <sakukumori@gmail.com>
2026-07-28 13:07:39 +0900

    Update main.py

--- app.py/main.py
+++ app.py/main.py
@@ -30,7 +30,7 @@ if st.button("送信する"):
     else:
         with st.spinner("AI先生が考え中..."):
             try:
-                model = genai.GenerativeModel('gemini-1.5-flash')
+                model = genai.GenerativeModel('models/gemini-1.5-flash')
 
                 # 送信するプロンプトの準備
                 prompt_parts = []
